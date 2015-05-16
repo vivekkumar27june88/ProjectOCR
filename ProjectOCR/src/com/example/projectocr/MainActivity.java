f8ef2359787b7200eb1ed4019ba40de3d2baa463
Vivek Kumar <dearvivekkumar@gmail.com>
2015-05-16 15:58:38 +0530

    1. Added code for selecting image from filesystem locally. 2. Minor changes with the main activity UI layout

--- ProjectOCR/src/com/example/projectocr/MainActivity.java
+++ ProjectOCR/src/com/example/projectocr/MainActivity.java
@@ -9,28 +9,37 @@
 package com.example.projectocr;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.Menu;
-import android.view.View;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-	private ArrayList<File> imageFileList;
-	private String imageDirPath;
+	private ArrayList<File> locImageFileList = null;
+	private String imageDirPath = null;
 	private ImageView imageView = null;
 	private Button prevButton = null;
-	private Button startCameraButton = null;
 	private Button nextButton = null;
+	private Button startCameraButton = null;
+	private Button selectImagebutton = null;
 	private int curSelectedIndex = -1;
+	private static final int REQUEST_CODE = 1;
 
 	private int getCurSelectedIndex() {
 
@@ -44,7 +53,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	}
 
 	private enum Naviagation {
-		NEXT, PREV, NO_CHANGE
+		NEXT, PREV, LAST, NO_CHANGE
 	};
 
 	private void init() {
@@ -54,20 +63,23 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		imageDirPath = Environment.getExternalStoragePublicDirectory(
 				Environment.DIRECTORY_DOWNLOADS).getAbsolutePath()
 				+ File.separator + "projectOCR";
-		this.imageFileList = new ArrayList<File>();
+		this.locImageFileList = new ArrayList<File>();
 		populateImageFileList();
 
 		this.prevButton = (Button) findViewById(R.id.prev_button);
 		this.prevButton.setOnClickListener(this);
 
-		this.startCameraButton = (Button) findViewById(R.id.start_camera_button);
-		this.startCameraButton.setOnClickListener(this);
-
 		this.nextButton = (Button) findViewById(R.id.next_button);
 		this.nextButton.setOnClickListener(this);
 
+		this.startCameraButton = (Button) findViewById(R.id.start_camera_button);
+		this.startCameraButton.setOnClickListener(this);
+
+		this.selectImagebutton = (Button) findViewById(R.id.selectImagebutton);
+		this.selectImagebutton.setOnClickListener(this);
+
 		this.imageView = (ImageView) findViewById(R.id.imageView1);
-		if (this.imageFileList.isEmpty() == false) {
+		if (this.locImageFileList.isEmpty() == false) {
 			this.setCurSelectedIndex(0);
 			changeImage(MainActivity.Naviagation.NO_CHANGE);
 		}
@@ -84,7 +96,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 			Log.d("VIVEK", "directory already present");
 		}
 
-		imageFileList.clear();
+		locImageFileList.clear();
 		File[] files = imageDir.listFiles();
 
 		for (File file : files) {
@@ -92,7 +104,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 				if (file.getName().endsWith(".png") == true
 						|| file.getName().endsWith(".jpg") == true
 						|| file.getName().endsWith(".jpeg") == true) {
-					imageFileList.add(file);
+					locImageFileList.add(file);
 				}
 			}
 		}
@@ -135,30 +147,74 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		case R.id.start_camera_button:
 			break;
 
+		case R.id.selectImagebutton:
+			pickImageFromDevice();
+			break;
+
 		case R.id.next_button:
 			changeImage(MainActivity.Naviagation.NEXT);
 			break;
 		}
 	}
 
+	private void pickImageFromDevice() {
+
+		Intent intent = new Intent();
+		intent.setType("image/*");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		startActivityForResult(intent, REQUEST_CODE);
+	}
+
+	@Override protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+		if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+			Uri uri = intent.getData();
+			if (uri.getScheme().toString().compareTo("content") == 0) {
+				Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+				if (cursor.moveToFirst()) {
+					int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+					Uri filePathUri = Uri.parse(cursor.getString(column_index));
+					String filePath = filePathUri.getPath();
+					Log.d("VIVEK", "File Name & PATH are:" + filePath);
+					File file = new File(filePath);
+					if (file.isFile() == true) {
+						if (file.getName().endsWith(".png") == true
+								|| file.getName().endsWith(".jpg") == true
+								|| file.getName().endsWith(".jpeg") == true) {
+							this.locImageFileList.add(file);
+							this.changeImage(MainActivity.Naviagation.LAST);
+						}
+					}
+				}
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, intent);
+	}
+
 	private void changeImage(Naviagation nav) {
 
 		if ((nav == MainActivity.Naviagation.NEXT)
-				&& (this.getCurSelectedIndex() < this.imageFileList.size() - 1)) {
+				&& (this.getCurSelectedIndex() < this.locImageFileList.size() - 1)) {
 			this.setCurSelectedIndex(this.curSelectedIndex + 1);
 		}
 		else if (nav == MainActivity.Naviagation.PREV && this.getCurSelectedIndex() > 0) {
 			this.setCurSelectedIndex(this.curSelectedIndex - 1);
 		}
+		else if (nav == MainActivity.Naviagation.LAST) {
+			this.setCurSelectedIndex(this.locImageFileList.size() - 1);
+		}
+		
 
-		Bitmap bitmap = BitmapFactory.decodeFile(this.imageFileList.get(this.getCurSelectedIndex())
-				.toString());
+		Bitmap bitmap = BitmapFactory.decodeFile(this.locImageFileList.get(
+				this.getCurSelectedIndex()).toString());
 		this.imageView.setImageBitmap(bitmap);
 	}
 
 	private void setButtonState() {
 
 		this.prevButton.setEnabled(this.getCurSelectedIndex() != 0);
-		this.nextButton.setEnabled(this.getCurSelectedIndex() != (this.imageFileList.size() - 1));
+		this.nextButton
+				.setEnabled(this.getCurSelectedIndex() != (this.locImageFileList.size() - 1));
 	}
 }
